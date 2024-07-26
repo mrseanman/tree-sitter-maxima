587d0ee4bdc01f33ccc80cbefa5e06e4926ee075
Sean O'Brien <seancanobrien@hotmail.co.uk>
2024-07-26 12:00:07 +0100

    Working quite well!

--- grammar.js
+++ grammar.js
@@ -1,54 +1,103 @@
-const PREC = {
-  ASSIGN: -10,
-  ASSIGN_EVAL: -10,
-  RIGHT_ASSIGN: -10,
-  SUBTRACTION: 15,
-  ADDITION: 100,
-  UNARY_MINUS: 25,
-  MULTIPLICATION: 30,
-  DIVISION: 35,
-  POWER: 40,
-  FACTORIAL: 45,
+const OPPREC = {
+  ASSIGN: 10,
+  ASSIGN_EVAL: 20,
+  ASSIGN_FUNCTION: 30,
+  IF: 40,
+  EQUALITY: 50,
+  INEQUALITY: 60,
+  SUBTRACTION: 70,
+  ADDITION: 80,
+  UNARY_MINUS: 90,
+  MULTIPLICATION: 100,
+  DIVISION: 110,
+  EXPONENT: 120,
+  FACTORIAL: 130,
+  INDEX: 140,
+};
+
+// Maybe get rid of this stuff
+const TYPEPREC = {
+  AMBIGUOUS: -1000,
+  COMPOUND: -1000,
+  LOGICAL: -1000,
+  ALGEBRAIC: -1000,
+  ASSIGNMENT: -1000,
 };
 
 module.exports = grammar({
   name: "maxima",
+
   extras: ($) => [$.comment, $._whitespace],
+
   rules: {
     source_file: ($) => repeat($.statement),
+    statement: ($) => seq($._expression, choice("$", ";")),
 
-    statement: ($) => seq($.expression, choice("$", ";")),
+    // *************************************************************************
+    // *************************************************************************
+    // ******* EXPRESSIONS *****************************************************
+    // *************************************************************************
+    // *************************************************************************
 
-    // -------------------------------------------------------------------------
-
-    expression: ($) =>
+    _expression: ($) =>
       choice(
-        $._bracket_expression,
-        $.assign,
-        $.assign_eval,
-        $._atom,
-        $._binary_function,
-        $._nary_function,
-        $._unary_function,
-        $.function_call,
-        $.list,
-        $.block,
-        // $.if,
-        // $.for,
-        // $.while,
+        // Ambiguous type
+        $.expression,
+        // Known, algebraic type
+        $.algebraic_expression,
+        // Known, logical type
+        $.logical_expression,
+        $.string_literal,
       ),
 
-    // -------------------------------------------------------------------------
+    // *************************************************************************
+    // ******* AMBIGUOUS TYPE **************************************************
+    // *************************************************************************
 
-    assign: ($) => prec(PREC.ASSIGN, seq($.identifier, ":", $.expression)),
+    expression: ($) =>
+      prec(
+        TYPEPREC.AMBIGUOUS,
+        choice(
+          $._ambiguous_atom,
+          $._bracket_ambiguous_expression,
+          $._assignment_expression,
+          $.array,
+          $.indexed_array,
+          $.block,
+          $.function_call,
+          $.if,
+        ),
+      ),
 
-    assign_eval: ($) =>
-      prec(PREC.ASSIGN_EVAL, seq($.identifier, "::", $.expression)),
+    _bracket_ambiguous_expression: ($) => seq("(", $.expression, ")"),
 
-    // TODO constants such as %pi or %e
-    _atom: ($) => choice($.identifier, $.number),
+    block: ($) => seq("block", "(", optional($._function_arguments), ")"),
 
-    _bracket_expression: ($) => seq("(", $.expression, ")"),
+    // -------------------------------------------
+
+    if: ($) =>
+      prec.right(
+        OPPREC.IF,
+        seq(
+          "if",
+          $._condition_then_consequence,
+          repeat(field("alternative", $.elseif_alternative)),
+          optional(field("alternative", $.else_alternative)),
+        ),
+      ),
+
+    elseif_alternative: ($) => seq("elseif", $._condition_then_consequence),
+
+    else_alternative: ($) => seq("else", field("consequence", $._expression)),
+
+    _condition_then_consequence: ($) =>
+      seq(
+        field("condition", choice($.expression, $.logical_expression)),
+        "then",
+        field("consequence", $._expression),
+      ),
+
+    // -------------------------------------------
 
     function_call: ($) =>
       seq(
@@ -58,61 +107,281 @@ module.exports = grammar({
         ")",
       ),
 
-    _function_arguments: ($) =>
-      seq(
-        repeat(seq(field("argument", $.expression), ",")),
-        field("argument", $.expression),
+    _ambiguous_atom: ($) => choice($.identifier),
+
+    // *************************************************************************
+    // ******* ASSIGNMENT TYPE *************************************************
+    // *************************************************************************
+
+    _assignment_expression: ($) =>
+      prec(
+        TYPEPREC.ASSIGNMENT,
+        choice(
+          $._bracket_assignment_expression,
+          $.assign,
+          $.assign_eval,
+          $.assign_function,
+        ),
       ),
 
-    list: ($) => seq("[", optional($._function_arguments), "]"),
+    _bracket_assignment_expression: ($) =>
+      seq("(", $._assignment_expression, ")"),
 
-    block: ($) => seq("block", "(", optional($._function_arguments), ")"),
+    assign: ($) =>
+      prec(
+        OPPREC.ASSIGN,
+        seq(
+          field("assigned_identifier", $.identifier),
+          ":",
+          field("assigned_value", $._expression),
+        ),
+      ),
 
-    // -------------------------------------------------------------------------
+    assign_eval: ($) =>
+      prec(OPPREC.ASSIGN_EVAL, seq($.identifier, "::", $._expression)),
 
-    _unary_function: ($) => choice($.unary_minus),
+    assign_function: ($) =>
+      prec(
+        OPPREC.ASSIGN_FUNCTION,
+        seq(
+          field("assigned_identifier", $.identifier),
+          "(",
+          optional($._function_arguments),
+          ")",
+          ":=",
+          field("assigned_value", $._expression),
+        ),
+      ),
 
-    unary_minus: ($) => prec(PREC.UNARY_MINUS, seq("-", $.expression)),
+    // *************************************************************************
+    // ******* COMPOUND TYPES **************************************************
+    // *************************************************************************
 
-    // -------------------------------------------------------------------------
+    array: ($) => seq("[", optional($._function_arguments), "]"),
+    indexed_array: ($) =>
+      prec(
+        OPPREC.INDEX,
+        seq(
+          field("array", $.expression),
+          "[",
+          field("index", choice($.expression, $.algebraic_expression)),
+          "]",
+        ),
+      ),
 
-    _binary_function: ($) => choice($.subtraction, $.division),
+    // *************************************************************************
+    // ******* ALGEBRAIC TYPE **************************************************
+    // *************************************************************************
 
-    subtraction: ($) =>
-      prec.left(PREC.SUBTRACTION, seq($.expression, "-", $.expression)),
+    algebraic_expression: ($) =>
+      prec(
+        TYPEPREC.ALGEBRAIC,
+        choice(
+          $._algebraic_atom,
+          $._bracket_algebraic_expression,
+          $._binary_algebraic_function,
+          $._unary_algebraic_function,
+        ),
+      ),
 
-    division: ($) =>
-      prec.left(PREC.DIVISION, seq($.expression, "/", $.expression)),
+    _bracket_algebraic_expression: ($) => seq("(", $.algebraic_expression, ")"),
 
-    // -------------------------------------------------------------------------
+    _algebraic_atom: ($) => choice($.number, $._const),
 
-    _nary_function: ($) => choice($.multiplication, $.addition),
+    _const: ($) => choice($.pi_const_atom, $.e_const_atom),
+
+    // -------------------------------------------
+
+    _unary_algebraic_function: ($) => choice($.unary_minus),
+
+    unary_minus: ($) =>
+      prec(
+        OPPREC.UNARY_MINUS,
+        seq("-", choice($.expression, $.algebraic_expression)),
+      ),
+
+    // -------------------------------------------
+
+    _binary_algebraic_function: ($) =>
+      choice(
+        $.addition,
+        $.subtraction,
+        $.multiplication,
+        $.division,
+        $.exponent,
+      ),
 
     addition: ($) =>
-      prec(
-        PREC.ADDITION,
+      prec.left(
+        OPPREC.ADDITION,
         seq(
-          repeat1(seq(field("summand", $.expression), "+")),
-          field("summand", $.expression),
+          field("summand", choice($.expression, $.algebraic_expression)),
+          "+",
+          field("summand", choice($.expression, $.algebraic_expression)),
+        ),
+      ),
+
+    subtraction: ($) =>
+      prec.left(
+        OPPREC.SUBTRACTION,
+        seq(
+          field("summand", choice($.expression, $.algebraic_expression)),
+          "-",
+          field("subtractand", choice($.expression, $.algebraic_expression)),
         ),
       ),
 
     multiplication: ($) =>
-      prec(
-        PREC.MULTIPLICATION,
+      prec.left(
+        OPPREC.MULTIPLICATION,
         seq(
-          repeat1(seq(field("multiplicand", $.expression), "*")),
-          field("multiplicand", $.expression),
+          field("multiplicand", choice($.expression, $.algebraic_expression)),
+          "*",
+          field("multiplicand", choice($.expression, $.algebraic_expression)),
         ),
       ),
 
-    //-------------------------------------------------------------------------
+    division: ($) =>
+      prec.left(
+        OPPREC.DIVISION,
+        seq(
+          field("numerator", choice($.expression, $.algebraic_expression)),
+          "/",
+          field("divisor", choice($.expression, $.algebraic_expression)),
+        ),
+      ),
+
+    exponent: ($) =>
+      prec.right(
+        OPPREC.EXPONENT,
+        seq(
+          field("base", choice($.expression, $.algebraic_expression)),
+          "^",
+          field("exponent", choice($.expression, $.algebraic_expression)),
+        ),
+      ),
+
+    // -------------------------------------------
+
+    pi_const_atom: ($) => token("%pi"),
+    e_const_atom: ($) => token("%e"),
+
+    // *************************************************************************
+    // ******* LOGICAL TYPE ****************************************************
+    // *************************************************************************
+
+    logical_expression: ($) =>
+      prec(
+        TYPEPREC.LOGICAL,
+        choice(
+          $._bracket_logical_expression,
+          $._logical_atom,
+          $.equation,
+          $.less_than,
+          $.less_than_eq,
+          $.greater_than,
+          $.greater_than_eq,
+        ),
+      ),
+
+    _bracket_logical_expression: ($) => seq("(", $.logical_expression, ")"),
+
+    _logical_atom: ($) => choice($._tf_atom),
+
+    _tf_atom: ($) => choice($.true_atom, $.false_atom),
+
+    // Equations are valid with really anything on LHS or RHS
+    // (sort of ... a=b=c is not parsed, but (a=b)=c and a=(b=c) are)
+    equation: ($) =>
+      prec.left(
+        OPPREC.EQUALITY,
+        seq(field("LHS", $._expression), "=", field("RHS", $._expression)),
+      ),
+
+    // For an inequality, only ambiguous or algebraic expressions can be LHS or RHS
+    less_than: ($) =>
+      prec.left(
+        OPPREC.INEQUALITY,
+        seq(
+          field("LHS", choice($.expression, $.algebraic_expression)),
+          "<",
+          field("RHS", choice($.expression, $.algebraic_expression)),
+        ),
+      ),
+    less_than_eq: ($) =>
+      prec.left(
+        OPPREC.INEQUALITY,
+        seq(
+          field("LHS", choice($.expression, $.algebraic_expression)),
+          "<=",
+          field("RHS", choice($.expression, $.algebraic_expression)),
+        ),
+      ),
+    greater_than: ($) =>
+      prec.right(
+        OPPREC.INEQUALITY,
+        seq(
+          field("LHS", choice($.expression, $.algebraic_expression)),
+          ">",
+          field("RHS", choice($.expression, $.algebraic_expression)),
+        ),
+      ),
+    greater_than_eq: ($) =>
+      prec.left(
+        OPPREC.INEQUALITY,
+        seq(
+          field("LHS", choice($.expression, $.algebraic_expression)),
+          ">=",
+          field("RHS", choice($.expression, $.algebraic_expression)),
+        ),
+      ),
+
+    // -------------------------------------------
+
+    true_atom: ($) => token("true"),
+    false_atom: ($) => token("false"),
+
+    // *************************************************************************
+    // ******* STRINGS *********************************************************
+    // *************************************************************************
+
+    string_literal: ($) =>
+      choice($._double_quoted_string, $._single_quoted_string),
+
+    _double_quoted_string: ($) =>
+      seq('"', repeat(choice($.escaped_char, $.double_quoted_content)), '"'),
+
+    _single_quoted_string: ($) =>
+      seq("'", repeat(choice($.escaped_char, $.single_quoted_content)), "'"),
+
+    escaped_char: ($) => token(seq("\\", /./)),
+
+    double_quoted_content: ($) => token(prec(-1, /[^"\\]+/)),
+
+    single_quoted_content: ($) => token(prec(-1, /[^'\\]+/)),
+
+    // *************************************************************************
+    // ******* HELPER **********************************************************
+    // *************************************************************************
+
+    _function_arguments: ($) =>
+      seq(
+        repeat(seq(field("argument", $._expression), ",")),
+        field("argument", $._expression),
+      ),
+
+    // *************************************************************************
+    // ******* PRIMITIVES ******************************************************
+    // *************************************************************************
 
     identifier: ($) => /[a-zA-Z_][a-zA-Z0-9_]*/,
 
     number: ($) => /\d+(\.\d+)?/,
 
-    //-------------------------------------------------------------------------
+    // *************************************************************************
+    // ******* EXTRAS **********************************************************
+    // *************************************************************************
+
     // TODO Nested comments are allowed (but not supported by this parser)
     comment: ($) => seq("/*", optional($._comment_text), "*/"),
 
---
+++ test_file.c
@@ -0,0 +1,20 @@
+int main() {
+    int number;
+
+    // Prompt the user to enter a number
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    // Use an if statement to check if the number is positive, negative, or zero
+    if (12) {
+        printf("The number is positive.\n");
+    } else if (number < 0) {
+        printf("The number is negative.\n");
+    } else {
+        printf("The number is zero.\n");
+    }
+
+    return 0;
+}
+int 1[a];
+int yo
